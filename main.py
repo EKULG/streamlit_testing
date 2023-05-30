a0536be1481cfa5952ae5bcc125d64045b36e80a
Luke G <luke@Lukes-MacBook-Pro-2.local>
2023-05-30 18:31:17 +0400

    dagag

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-# import streamlit as st
-# import io
-# from PIL import Image
+from streamlit_webrtc import (
+    VideoProcessorBase,
+    webrtc_streamer,
+    VideoHTMLAttributes
+)
+import threading
+from aiortc.contrib.media import MediaRecorder
+from video import find_face
 
-# img_file_buffer = st.camera_input("Take a picture")
+#Defining the variables
+lock = threading.Lock()
+img_container = {"frames":[]}
 
-# if img_file_buffer is not None:
-#     # To read image file buffer as bytes:
-#     bytes_data = img_file_buffer.getvalue()
-#     img = Image.open(io.BytesIO(bytes_data))
-#     img.show()
-    
+s_per_question=60 #maximum duration of the video input in s
 
-#     # Check the type of bytes_data:
-#     # Should output: <class 'bytes'>
-#     # st.write(type(bytes_data))
-    
-    
-    
-from streamlit_webrtc import webrtc_streamer
-webrtc_streamer(key="sample")
+
+#Extracting frames from the video input
+class VideoProcessor(VideoProcessorBase):
+    def __init__(self):
+        self.style = 'color'
+
+    def video_frame_callback(self):
+        img = self.to_ndarray(format="bgr24")
+        img = find_face(img)
+        with lock:
+            if img is not None:
+                img_container["frames"].append(img)
+
+
+# var = MediaRecorder("record.mp3")
+
+webrtc_streamer(
+            key="object-detection",
+            # video_frame_callback=VideoProcessor.video_frame_callback,
+            media_stream_constraints={
+                "video": True,
+                "audio": True
+            },
+            desired_playing_state=True #link to Start/Stop button
+            # , in_recorder_factory=var
+            , video_html_attrs = VideoHTMLAttributes(muted=True, volume=0, autoPlay=True, controls=False, stop=False #unabling the user to control video input
+                                                    #,style={"border": "5px red solid", "margin": "0 auto", "width":"50%"}
+                                                    )
+            ,  rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                            }
+        )
