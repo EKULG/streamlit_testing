2f7c963caabd311461311f37a6d02ed182e9469a
Luke G <luke@Lukes-MacBook-Pro-2.local>
2023-05-30 18:07:31 +0400

    update

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import streamlit as st
+# import streamlit as st
+# import io
+# from PIL import Image
 
-img_file_buffer = st.camera_input("Take a picture")
+# img_file_buffer = st.camera_input("Take a picture")
 
-if img_file_buffer is not None:
-    # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
-    # Check the type of bytes_data:
-    # Should output: <class 'bytes'>
-    st.write(type(bytes_data))
+# if img_file_buffer is not None:
+#     # To read image file buffer as bytes:
+#     bytes_data = img_file_buffer.getvalue()
+#     img = Image.open(io.BytesIO(bytes_data))
+#     img.show()
+    
+
+#     # Check the type of bytes_data:
+#     # Should output: <class 'bytes'>
+#     # st.write(type(bytes_data))
+    
+    
+    
+from streamlit_webrtc import webrtc_streamer
+webrtc_streamer(key="sample")
